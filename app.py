217eaa48fdb26082917d1e96616a45f69269c407
kailetkl <99379474+kailetkl@users.noreply.github.com>
2022-04-06 22:44:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[9]:
 
 
 from flask import Flask
 
 
-# In[11]:
+# In[10]:
 
 
 app = Flask(__name__)
 
 
-# In[12]:
+# In[11]:
 
 
 from flask import request, render_template
@@ -91,7 +91,7 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="127.0.0.1", port=int("80"))
 
 
 # In[ ]:
